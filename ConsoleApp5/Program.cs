3ed167b4c19315cba512a95093b9509f8b663710
Larry Milbank <MIL202006635@longroad.ac.uk>
2021-01-26 09:51:22 +0000

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Hokemon_U
+{
+    class Hokemon
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Hokeworld!");
+
+            Hokemon hokeObject01 = new Hokemon();
+
+            //Console.Writeline("Hokemon name is: {0}", hokeobject.Name);
+
+            hokeObject01.get_details();
+
+            Hokemon hoke02 = new Hokemon(); 
+
+            hokeObject01.get_details();
+        }   
+    }
+}
+namespace Hokemon_T
+{
+    class Hokemon
+    {
+        // MEMBERS - All Attributes, Methods and Properties are MEMBERS of this CLASS
+
+        // VARIABLES a.k.a ATTRIBUTES inside a class can also be refered to as FIELDS
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+
+        //Properties
+
+        public string Name
+        {
+            get { return name; } // access to the name attribute
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; } // access to the health attribute
+            set { health = value; }
+        }
+
+        //Methods
+
+        public Hokemon()   // CONSTRUCTOR method runs 1st when INSTANTIATED
+        {
+            provide_name();
+            max_health = 100;
+            health = Random_int_generator(10, max_health);
+            attack = Random_int_generator(10, 100);
+            speed = Random_int_generator(10, 100);
+            defense = Random_int_generator(10, 100);
+
+        }
+
+
+        public void provide_name() // METHOD
+        {
+            Console.WriteLine("Enter a name for your Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+
+        public void get_details()
+        {
+            Console.WriteLine("\n\n*********\nName: {0}\n" +
+                "Health: {1}/{2}\n" +
+                "Attack value: {3}\n" +
+                "Speed value: {4}\n" +
+                "Defense: {5}", name, Health, max_health, attack, speed, defense);
+        }
+
+
+        public int Random_int_generator(int min_value, int max_value)
+        {
+            Random rnd = new Random();  // Creating a new OBJECT called rnd this is an
+                                        // INSTANCE of RANDOM class
+
+            int randomValue;
+
+            randomValue = rnd.Next(min_value, max_value);   // Using the NEXT method from the RANDOM class found in rnd OBJECT
+
+            return randomValue;
+        }
+
+
+        public void about()
+        {
+        Console.WriteLine("I am a mystical species known as Hokemon!!");
+        }
+        
+
+        }
+    }
+
+
+
+
+
+        
