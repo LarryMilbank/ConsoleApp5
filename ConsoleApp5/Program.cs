8b3fa8aeba0b7666fb0d51b4079cc9273de849eb
Gareth Adams <gadams@longroad.ac.uk>
2021-01-26 10:00:49 +0000

    Organising namespace name
    Creating Hokemon Class

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -2,17 +2,18 @@
 
 namespace Hokemon_U
 {
-    class Hokemon
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Hokeworld!");
 
-            Hokemon hokeObject01 = new Hokemon();
+            Hokemon hokeObject01 = new Hokemon(); // INSTANTIATE hokeObject from HOKEMON class
 
             //Console.Writeline("Hokemon name is: {0}", hokeobject.Name);
 
             hokeObject01.get_details();
+            hokeObject01.about();
 
             Hokemon hoke02 = new Hokemon(); 
 
@@ -20,87 +21,6 @@ namespace Hokemon_U
         }   
     }
 }
-namespace Hokemon_T
-{
-    class Hokemon
-    {
-        // MEMBERS - All Attributes, Methods and Properties are MEMBERS of this CLASS
-
-        // VARIABLES a.k.a ATTRIBUTES inside a class can also be refered to as FIELDS
-        private string name;
-        private int health;
-        private int max_health;
-        private int attack;
-        private int speed;
-        private int defense;
-
-        //Properties
-
-        public string Name
-        {
-            get { return name; } // access to the name attribute
-            set { name = value; }
-        }
-
-        public int Health
-        {
-            get { return health; } // access to the health attribute
-            set { health = value; }
-        }
-
-        //Methods
-
-        public Hokemon()   // CONSTRUCTOR method runs 1st when INSTANTIATED
-        {
-            provide_name();
-            max_health = 100;
-            health = Random_int_generator(10, max_health);
-            attack = Random_int_generator(10, 100);
-            speed = Random_int_generator(10, 100);
-            defense = Random_int_generator(10, 100);
-
-        }
-
-
-        public void provide_name() // METHOD
-        {
-            Console.WriteLine("Enter a name for your Hokemon: ");
-            name = Console.ReadLine();
-        }
-
-
-        public void get_details()
-        {
-            Console.WriteLine("\n\n*********\nName: {0}\n" +
-                "Health: {1}/{2}\n" +
-                "Attack value: {3}\n" +
-                "Speed value: {4}\n" +
-                "Defense: {5}", name, Health, max_health, attack, speed, defense);
-        }
-
-
-        public int Random_int_generator(int min_value, int max_value)
-        {
-            Random rnd = new Random();  // Creating a new OBJECT called rnd this is an
-                                        // INSTANCE of RANDOM class
-
-            int randomValue;
-
-            randomValue = rnd.Next(min_value, max_value);   // Using the NEXT method from the RANDOM class found in rnd OBJECT
-
-            return randomValue;
-        }
-
-
-        public void about()
-        {
-        Console.WriteLine("I am a mystical species known as Hokemon!!");
-        }
-        
-
-        }
-    }
-
 
 
 
